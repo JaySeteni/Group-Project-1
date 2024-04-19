1e59184fe3701ddaeba932805a05d982249ce895
Xola Bangaza <xolabangaza@gmail.com>
2024-04-19 14:35:19 +0200

    changess

--- server.js
+++ server.js
@@ -14,37 +14,21 @@ window.onload = async function () {
                 if (category === 'all' || product.category.toLowerCase() === category) {
                     const productDiv = document.createElement('div'); 
                     productDiv.classList.add('product');
-<<<<<<< HEAD
-                    productDiv.innerHTML = 
-                    `<img class="img" src="${product.image}" alt="${product.title}">
-                       
-=======
                     productDiv.innerHTML = `
-                    <div clss="image-container">    
+                    <div class="image-container">    
                     <img class="img" src="${product.image}" alt="${product.title}">
                    
->>>>>>> feature/Xola-Feature
                         <div class="cardBody">
                             <span>${product.title}</span>
                         </div>
                             
                         <div class="cardFooter">
                             <p class="price">R${product.price.toFixed(2)}</p> 
-<<<<<<< HEAD
-                           <button onclick="addtocart(${product})"><a href="../product/viewone.html?id=${product.id}">View More</a></button>
-                        </div>`;
-                    productDiv.innerHTML = `
-                        <img src="${product.image}" alt="${product.title}">
-                        <h3>${product.title}</h3>
-                        <p>R${product.price.toFixed(2)}</p> 
-                        <button onclick="addtocart(${product})"><a href="./viewone.html?id=${product.id}">Add To Cart</a></button>
-=======
                            <button><a href="./viewone.html?id=${product.id}">View More</a></button>
                         </div>
                         </div> 
                         
                         
->>>>>>> feature/Xola-Feature
                     `;
                     
                     productsList.appendChild(productDiv);
