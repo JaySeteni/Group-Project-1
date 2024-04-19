132acf042a142d2e6c0b09d932cce89d067f9036
JaySeteni <jay.seteni@gmail.com>
2024-04-19 04:04:50 +0200

    midnights

--- product/product.js
+++ product/product.js
@@ -41,3 +41,41 @@ addtocart({
         count: 400
     }
 })
+
+
+// jay
+
+const productContainer = document.querySelector("#product-container")
+const spinner = document.querySelector('#loading')
+async function 
+fetchAndDisplaySingleProduct(){
+    spinner.computedStyleMap.display = 'inline-block';
+    const productId = new URLSearchParams
+    (window.location.search).get("id");
+    const response = await fetch(
+    `https://fakestoreapi.com/products/${productId}`
+    );
+    const product = await response.json();
+    console.log(product, productId);
+    displayProduct(product);
+}
+
+function displayProduct(product){
+    spinner.computedStyleMap.display = 'none';
+    productContainer.innerHTML = `  
+    <div class="product-image">
+        <img src=${product.image} 
+        alt=${product.title} />
+    </div>
+    <p class="product-name">
+        ${product.title}    
+    </p>
+    <p class="product-description">
+     ${product.description}
+    </p>
+    <div class="detail">
+        ${product.price}
+    </div>
+</div>`
+}
+fetchAndDisplaySingleProduct();
