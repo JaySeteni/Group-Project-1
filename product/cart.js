5415a35940336f8b128d7812e660528b7ccb328f
JaySeteni <jay.seteni@gmail.com>
2024-04-22 16:05:04 +0200

    m

--- product/cart.js
+++ product/cart.js
@@ -13,38 +13,7 @@ window.onload = async ()=>{
         console.log(item)
         container.innerHTML =
          `
-         <div class="flex">
-            <div class="flex1">
-                <div class="product-image">
-                    <img src="${item.image}" 
-                    alt="${item.title}" />
-                </div>
-            </div>
-            <div class="flex2">
-                <p class="product-name">
-                ${item.title}
-                </p>
-                <div class="detail">
-                    R ${item.price}
-                </div>
-                <div class="cartButton">
-                        <button class="heart"> <i class="fa-regular fa-heart"></i></button>
-                        <button class="cart" onclick="addtocart(${item.item})"> add to cart</button>
-                    </div>
-                <p class="description">
-                ${item.description}
-                </p>
-                <div class="rating">
-                    <label class="rating-rate">
-                    ${item.rating.rate}
-                    <i class="fa-solid fa-star"></i>
-                    </label>
-                    <label class="rating-count">
-                        reviews(${item.rating.count})
-                    </label>
-                </div>
-            </div>
-        </div>`
+         `
         console.log(item)
 
     } catch (error) {
